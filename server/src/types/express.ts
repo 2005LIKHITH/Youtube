22cb9777fe515d399d107369a6ebb809d99beff4
2005LIKHITH <likhithcvsrl@gmail.com>
2024-11-18 22:57:49 +0530

    commited

--- server/src/types/express.ts
+++ server/src/types/express.ts
@@ -1,11 +1,8 @@
 
-
 import { IUser } from "../models/user.model";  // Import IUser (or User) model
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: IUser;  // Extend the Request interface to include 'user'
-    }
-  }
+declare module 'express' {
+  export interface Request {
+    user?: IUser;  
+}
 }
